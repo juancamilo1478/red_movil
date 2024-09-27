466c5fc438a0fc9776a4d6debe021d1b9f8d48fe
juanc <juancamilo1478@gmail.com>
2024-09-27 03:58:13 -0500

    save

--- src/components/Home_Components/Nav_bar.vue
+++ src/components/Home_Components/Nav_bar.vue
@@ -6,22 +6,30 @@
             <img src="/src/images/LOGO REDMOVIL.png" alt="image" class="max-w-[200px]" />
           
         </div>
-        <label for="menu-toggle" class="pointer-cursor md:hidden block">
-          <svg class="fill-current text-gray-900"
+        <label for="menu-toggle" class="pointer-cursor md:hidden block" @click="toggleMenu">
+          <svg class="fill-current text-gray-950"
             xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
             <title>menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
           </svg>
         </label>
         <input class="hidden" type="checkbox" id="menu-toggle" />
-    
+         <!-- Overlay -->
+         <div v-if="menuOpen" class="fixed inset-0 bg-black opacity-95 z-40 text-white font-bold flex items-center justify-center " @click="toggleMenu">
+            <ul class="flex flex-col items-center "> <!-- Utilizar flex-col para centrar verticalmente -->
+                <li class="mb-4"><a class="md:p-4 py-3 px-0 block text-5xl" href="#">Productos</a></li>
+                <li class="mb-4"><a class="md:p-4 py-3 px-0 block text-5xl" href="#">Video ayudas</a></li>
+                <li class="mb-4"><a class="md:p-4 py-3 px-0 block text-5xl" href="#">Contacto</a></li>
+                <li><a class="md:p-4 py-3 px-0 block text-5xl" href="#">Soy proveedor</a></li>
+            </ul>
+        </div>
         <div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
             <nav>
-                <ul class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
-                    <li><a class="md:p-4 py-3 px-0 block" href="#">Productos</a></li>
-                    <li><a class="md:p-4 py-3 px-0 block" href="#">Video ayudas</a></li>
-                    <li><a class="md:p-4 py-3 px-0 block" href="#">Contacto</a></li>
-                    <li><a class="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#">Soy provedor</a></li>
+                <ul class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0 ">
+                    <li><a class="md:p-4 py-3 px-0 font-semibold  " href="#">Productos</a></li>
+                    <li><a class="md:p-4 py-3 px-0 font-semibold" href="#">Video ayudas</a></li>
+                    <li><a class="md:p-4 py-3 px-0 font-semibold" href="#">Contacto</a></li>
+                    <li><a class="md:p-4 py-3 px-0 font-semibold md:mb-0 mb-2" href="#">Soy provedor</a></li>
                 </ul>
             </nav>
         </div>
@@ -30,6 +38,16 @@
 <script>
  
 export default {
-    name:'Nav_bar'
+    name:'Nav_bar',
+    data() {
+        return {
+            menuOpen: false // Estado para controlar la visibilidad del menú
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.menuOpen = !this.menuOpen; // Alternar el estado del menú
+        }
+    }
 }
 </script>
